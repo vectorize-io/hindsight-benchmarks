f5281e4d6a3a316a2271d7c089f26ff71237e331
Nicolò Boschi <boschi1997@gmail.com>
2026-04-01 13:22:40 +0200

    Fix static export for sitemap and robots routes

--- visualizer/app/sitemap.ts
+++ visualizer/app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next'
 
+export const dynamic = 'force-static'
+
 const siteUrl = 'https://benchmarks.hindsight.vectorize.io'
 
 export default function sitemap(): MetadataRoute.Sitemap {
